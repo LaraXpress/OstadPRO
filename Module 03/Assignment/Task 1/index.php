d4ea11ff00e0dbf19cc4c3dacf9bad36191c72ec
Mazedur <89848156+LaraXpress@users.noreply.github.com>
2023-02-13 23:16:54 +0600

    function declaration

---
+++ Module 03/Assignment/Task 1/index.php
@@ -0,0 +1,10 @@
+<?php
+// Write a Reusable  PHP Function that can check Even & Odd Number And Return Decision
+function isEvenOrOdd($num){
+  if( $num % 2 == 0){
+    return 1; 
+  }else{
+    return 0; 
+  }
+}
+?>
